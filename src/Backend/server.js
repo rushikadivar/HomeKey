d141e211686035ef216032c8ca1b0116cff3c1fb
rushikadivar <rushikadivar2701@gmail.com>
2021-05-28 11:21:25 +0530

    some fixes

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,13 +1,7 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const  dd  = require('./model/user')
-
-
-const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/Homekey-database', {
     useNewUrlParser: true,
@@ -19,83 +13,12 @@ const app = express()
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 
-// ////////////////////////   regoster the user ///////////////////////////////////////////////
+const userSigninRouter = require('./routes/Signin');
+const userSignupRouter = require('./routes/Signup');
 
-app.post('/register', async (req, res) => {
-    const { firstName, lastName, email, password: plainTextPassword } = req.body
-
-    if (!firstName || typeof firstName !== 'string') {
-        return res.json({ status: 'error', error: 'Invalid username' })
-    }
-    if (!lastName || typeof lastName !== 'string') {
-        return res.json({ status: 'error', error: 'Invalid username' })
-    }
-    if (!email || typeof email !== 'string') {
-        return res.json({ status: 'error', error: 'Invalid username' })
-    }
-
-    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-        return res.json({ status: 'error', error: 'Invalid password' })
-    }
-
-    if (plainTextPassword.length < 5) {
-        return res.json({
-            status: 'error',
-            error: 'Password too small. Should be atleast 6 characters'
-        })
-    }
-
-    const password = await bcrypt.hash(plainTextPassword, 10)
-
-    try {
-        const response = await dd.create({
-            firstName,
-            lastName,
-            email,
-            password
-        })
-        console.log('User created successfully: ', response)
-    } catch (error) {
-        if (error.code === 11000) {
-            // duplicate key
-            return res.json({ status: 'error', error: 'Username already in use' })
-        }
-        throw error
-    }
-
-    res.json({ status: 'ok' })
-})
-
-// ///////////// for login the user  ///////////////////
-
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body
-    const user = await dd.findOne({ username }).lean()
-
-    if (!user) {
-        return res.json({ status: 'error', error: 'Invalid username/password' })
-    }
-
-    if (await bcrypt.compare(password, user.password)) {
-        // the username, password combination is successful
-
-        const token = jwt.sign(
-            {
-                id: user._id,
-                username: user.username
-            },
-            JWT_SECRET
-        )
-
-        return res.json({ status: 'ok', data: token })
-    }
-
-    res.json({ status: 'error', error: 'Invalid username/password' })
-})
-
-
-
+app.use('/signin', userSigninRouter);
+app.use('/signup', userSignupRouter);
 
 app.listen(1010, () => {
     console.log('Server up at 1010')
-})
+});
